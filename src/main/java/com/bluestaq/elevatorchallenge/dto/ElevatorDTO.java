579f93758ddbc5e0335ead75e165aea80e418d31
Conor McCandless <cdmccandl@gmail.com>
2025-08-08 04:58:53 -0600

    added upward and downward sets to be returned as an elevator status

--- src/main/java/com/bluestaq/elevatorchallenge/dto/ElevatorDTO.java
+++ src/main/java/com/bluestaq/elevatorchallenge/dto/ElevatorDTO.java
@@ -5,11 +5,14 @@ import com.bluestaq.elevatorchallenge.service.ElevatorDoor;
 import com.bluestaq.elevatorchallenge.service.ElevatorMovement;
 
 import java.util.List;
+import java.util.Set;
 
 public record ElevatorDTO(
         int currentFloor,
         ElevatorMovement state,
         ElevatorDirection direction,
         ElevatorDoor doorState,
-        List<Integer> destinationFloors
+        List<Integer> destinationFloors,
+        Set<Integer> upwardDestinations,
+        Set<Integer> downwardDestinations
 ) {}
