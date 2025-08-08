ab064965ce55e5737be30f831aa86c912f0200ef
Conor McCandless <cdmccandl@gmail.com>
2025-08-07 23:37:28 -0600

    added rest of tests to unit test main elevator logic

--- src/test/java/com/bluestaq/elevatorchallenge/service/ElevatorContextTest.java
+++ src/test/java/com/bluestaq/elevatorchallenge/service/ElevatorContextTest.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * SpringBootTest that spins up and starts the service, I could do some controller layer testing here but for
+ * the sake of time I am using this to ensure the service starts without failure at the unit testing stage
+ */
 @SpringBootTest
-@Disabled
 public class ElevatorContextTest {
 
 
--- src/test/java/com/bluestaq/elevatorchallenge/service/ElevatorServiceTest.java
+++ src/test/java/com/bluestaq/elevatorchallenge/service/ElevatorServiceTest.java
@@ -4,6 +4,8 @@ import com.bluestaq.elevatorchallenge.exception.ElevatorEmergencyException;
 import com.bluestaq.elevatorchallenge.service.commands.CloseDoorsCommand;
 import com.bluestaq.elevatorchallenge.service.commands.OpenDoorsCommand;
 import com.bluestaq.elevatorchallenge.service.commands.PressButtonCommand;
+import jakarta.validation.constraints.AssertFalse;
+import jakarta.validation.constraints.AssertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -14,20 +16,27 @@ import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
+/**
+ * Unit tests for the service layer of this microservice. This is where most of the business logic resides
+ * For the sake of time I wrote mostly basic functionality testing and did not cover all edge cases. Using
+ * this as an example of how I format test names and strategies
+ */
 @ExtendWith(MockitoExtension.class)
 public class ElevatorServiceTest {
 
     @Spy
     ElevatorState elevator;
-
     @Spy
     ElevatorDestinationManager destinationManager;
-
     @Mock
     SafetyValidator safetyValidator;
-
+    //have to do this type of spy instantiation in order to inject relevant dependencies into dependent classes for
+    //class under test
     @InjectMocks
     private OpenDoorsCommand openDoorsCommand = Mockito.spy(OpenDoorsCommand.class);
 
@@ -43,7 +52,6 @@ public class ElevatorServiceTest {
 
     @BeforeEach
     void setUp() {
-
         // Reset elevator to default state for each test
         elevator.setCurrentFloor(1);
         elevator.setDirection(ElevatorDirection.NONE);
@@ -51,19 +59,25 @@ public class ElevatorServiceTest {
         elevator.setCurrentDoorState(ElevatorDoor.CLOSED);
         elevator.setDoorOperationStartTimeMs(-1);
         elevator.setMovementOperationStartTimeMs(-1);
-
         // Clear destinations
         destinationManager.clearAllDestinations();
     }
 
+    // =================== Door State Tests ================================
     @Test
     public void testPressOpenDoorCommandWhenClosed() {
-
         Mockito.doReturn(true).when(safetyValidator).canOpenDoors(Mockito.any());
         elevatorService.openDoors();
         //run a loop of the main elevator loop
         elevatorService.processElevatorOperations();
         assertEquals(ElevatorDoor.OPENING, elevatorService.getCurrentElevatorState().doorState());
+    }
+
+    @Test
+    public void testPressOpenDoorCommandWhenMoving() {
+        Mockito.doReturn(false).when(safetyValidator).canOpenDoors(Mockito.any());
+        elevator.setCurrentMovementState(ElevatorMovement.MOVING);
+        assertThrows(IllegalArgumentException.class, () -> elevatorService.openDoors());
     }
 
     @Test
@@ -74,34 +88,14 @@ public class ElevatorServiceTest {
         });
     }
 
+    // =================== Floor Button Tests ================================
     @Test
-    void testOpenDoorsWhenEmergencyModeThrowsException() {
-        // setup
-        elevator.setCurrentMovementState(ElevatorMovement.EMERGENCY);
-        assertThrows(ElevatorEmergencyException.class, () -> elevatorService.openDoors());
-    }
+    public void testPressFloorButtonOnceWhileIdleState() {
 
-    @Test
-    void testCloseDoorsWhenEmergencyModeThrowsException() {
-        // setup
-        elevator.setCurrentMovementState(ElevatorMovement.EMERGENCY);
-        assertThrows(ElevatorEmergencyException.class, () -> elevatorService.closeDoors());
-    }
-
-    @Test
-    @Disabled
-    public void testPressFloorButtonWhileIdleState() {
-
-
-        //consume it from the command queue
         elevatorService.pressFloorButton(5);
         elevatorService.processElevatorOperations();
-
-        // add some floor requests to the queue
-
-        elevatorService.processElevatorOperations();
-
-
+        // check the floor queue is populated
+        assertFalse(elevatorService.destinationManager.getAllDestinations().isEmpty());
     }
 
     @Test
@@ -120,6 +114,85 @@ public class ElevatorServiceTest {
         Mockito.verify(pressButtonCommand, Mockito.times(0)).executeCommand(elevator);
     }
 
+    @Test
+    public void testElevatorMovesWhenFloorButtonPressed() {
+
+        elevatorService.pressFloorButton(5);
+        elevatorService.processElevatorOperations();
+        // check the floor queue is populated
+        assertSame(ElevatorDirection.UP, elevator.getDirection());
+    }
+
+    @Test
+    public void testElevatorClearsFloorRequestWhenTargetFloorReached() {
+
+        elevatorService.pressFloorButton(5);
+        elevatorService.processElevatorOperations();
+        // check the floor queue is populated
+        assertFalse(elevatorService.destinationManager.getAllDestinations().isEmpty());
+        //set current floor to 5 for test, we are skipping simulating the working loop
+        elevator.setCurrentFloor(5);
+        elevator.setCurrentMovementState(ElevatorMovement.IDLE);
+        elevatorService.processElevatorOperations();
+        //check that our destination queue is empty
+        assertTrue(elevatorService.destinationManager.getAllDestinations().isEmpty());
+    }
+
+    @Test
+    public void testElevatorDoesNotAddDuplicateFloorRequests() {
+        elevatorService.pressFloorButton(5);
+        elevatorService.pressFloorButton(5);
+        elevatorService.processElevatorOperations();
+        // check the floor queue is populated with ONE element
+        assertEquals(1, elevatorService.destinationManager.getAllDestinations().size());
+    }
+
+    // =================== Emergency Button Tests ================================
+    @Test
+    void testEmergencyStopClearsAllDestinationsAndSetsState() {
+        elevatorService.pressFloorButton(5);
+        elevatorService.processElevatorOperations();
+        elevatorService.pressFloorButton(7);
+        elevatorService.processElevatorOperations();
+        assertEquals(2, elevatorService.destinationManager.getAllDestinations().size());
+
+       //press emergency button
+        elevatorService.emergencyStop();
+        assertTrue(elevatorService.destinationManager.getAllDestinations().isEmpty());
+        assertEquals(ElevatorMovement.EMERGENCY,  elevator.getCurrentMovementState());
+    }
 
 
+    @Test
+    void testOpenDoorsWhenEmergencyModeThrowsException() {
+        // setup
+        elevator.setCurrentMovementState(ElevatorMovement.EMERGENCY);
+        assertThrows(ElevatorEmergencyException.class, () -> elevatorService.openDoors());
+    }
+
+    @Test
+    void testCloseDoorsWhenEmergencyModeThrowsException() {
+        // setup
+        elevator.setCurrentMovementState(ElevatorMovement.EMERGENCY);
+        assertThrows(ElevatorEmergencyException.class, () -> elevatorService.closeDoors());
+    }
+
+    @Test
+    void testPressButtonWhenEmergencyModeThrowsException() {
+        // setup
+        elevator.setCurrentMovementState(ElevatorMovement.EMERGENCY);
+        assertThrows(ElevatorEmergencyException.class, () -> elevatorService.pressFloorButton(1));
+    }
+
+    @Test
+    void testClearEmergencyRemovesEmergencyState() {
+        //press emergency button
+        elevatorService.emergencyStop();
+        //assert that an open doors command throws exception
+        assertThrows(ElevatorEmergencyException.class, () -> elevatorService.openDoors());
+        //clear the emergency
+        elevatorService.emergencyClear();
+        //exception is NOT thrown
+        assertDoesNotThrow(() -> elevatorService.pressFloorButton(5));
+    }
 }
